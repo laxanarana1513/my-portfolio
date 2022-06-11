1c26d3db0a6a81517b05144d15e9ac31f67bfa14
Laxana Rana <laxanarana1513@gmail.com>
2022-06-11 22:49:51 +0530

    COMMIT

--- src/components/Resume/Skills/Skills.js
+++ src/components/Resume/Skills/Skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Skills.css';
 // import { Fade } from 'react-reveal';
+import S1 from "../../../assests/images/S1.png"
 
 const skills = () => {
     return (
@@ -22,92 +23,30 @@ const skills = () => {
                 <div className="col-md-9">
                     <div className={classes.Item}>
                         <h4 className={classes.SubHead}>Here are the skills and technologies that I have worked with and has a indepth knowledge: </h4>
-
                         <div id="codeconSkills">
+                            <div className="row" >
 
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconHTML}>
-                                    <span className={classes.CodeconSkillArea}>HTML</span>
-                                    <span className={classes.PercentText}>100%</span>
+                                <div className={classes.Skill}>
+                                    <img src={S1} alt="" />
+                                </div>
+                                <div className={classes.Skill}>
+                                    <img src="" alt="" />
+                                </div>
+                                <div className={classes.Skill}>
+                                    <img src="" alt="" />
+                                </div>
+                                <div className={classes.Skill}>
+                                    <img src="" alt="" />
+                                </div>
+                                <div className={classes.Skill}>
+                                    <img src="" alt="" />
+                                </div>
+                                <div className={classes.Skill}>
+                                    <img src="" alt="" />
                                 </div>
                             </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconCSS}>
-                                    <span className={classes.CodeconSkillArea}>CSS/Bootstrap</span>
-                                    <span className={classes.PercentText}>98%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconReact}>
-                                    <span className={classes.CodeconSkillArea}>React JS</span>
-                                    <span className={classes.PercentText}>80%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconJS}>
-                                    <span className={classes.CodeconSkillArea}>Javascript</span>
-                                    <span className={classes.PercentText}>75%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconCC}>
-                                    <span className={classes.CodeconSkillArea}>C++ </span>
-                                    <span className={classes.PercentText}>89%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconC}>
-                                    <span className={classes.CodeconSkillArea}>C</span>
-                                    <span className={classes.PercentText}>89%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconJAVA}>
-                                    <span className={classes.CodeconSkillArea}>JAVA </span>
-                                    <span className={classes.PercentText}>65%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconPython}>
-                                    <span className={classes.CodeconSkillArea}>Python </span>
-                                    <span className={classes.PercentText}>50%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconSQL}>
-                                    <span className={classes.CodeconSkillArea}>SQL </span>
-                                    <span className={classes.PercentText}>90%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconAlgo}>
-                                    <span className={classes.CodeconSkillArea}>Algorithms</span>
-                                    <span className={classes.PercentText}>75%</span>
-                                </div>
-                            </div>
-
-                            <div className={classes.CodeconSkillbar}>
-                                <div className={classes.CodeconCyber}>
-                                    <span className={classes.CodeconSkillArea}>Cyber-Security</span>
-                                    <span className={classes.PercentText}>50%</span>
-                                </div>
-                            </div>
-
                         </div>
-
-
                     </div>
-
-
                 </div>
             </div>
 
