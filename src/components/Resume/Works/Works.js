9c76833d44002e7cedb4f7ce7b93eaf288783c56
Laxana Rana <laxanarana1513@gmail.com>
2022-06-13 17:58:09 +0530

    commits

--- src/components/Resume/Works/Works.js
+++ src/components/Resume/Works/Works.js
@@ -3,6 +3,7 @@ import classes from './Works.css';
 import Work1 from '../../../assests/images/W1.png';
 import Work2 from '../../../assests/images/W2.png';
 import Work3 from '../../../assests/images/W3.png';
+import Work4 from '../../../assests/images/W4.png';
 
 const works = () => {
     return (
@@ -10,69 +11,96 @@ const works = () => {
             <div className={classes.Row}>
                 <h1 className={classes.H1}>Check Out Some Of My Works...</h1>
                 <div className={classes.Wrap}>
-                    <div className={classes.Title}>
-                        <img src={Work2} alt="" className={classes.Timg} />
-                        <div className={classes.Text}>
-                            {/* <h1>Lorem ipsum.</h1> */}
-                            <h2 className={classes.Animate_Text}>Bootstrap Tour & Travels Frontend Website</h2>
-                            <p className={classes.Animate_Text}>
-                                <a href="https://github.com/laxanarana1513/travelwild" target="_blank" rel="noopener noreferrer">
-                                    <img src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png" alt="" />
-                                </a>
+                    <div className="row">
+                        <div className={classes.Title}>
+                            <img src={Work2} alt="" className={classes.Timg} />
+                            <div className={classes.Text}>
+                                {/* <h1>Lorem ipsum.</h1> */}
+                                <h2 className={classes.Animate_Text}>Bootstrap Tour & Travels Frontend Website</h2>
+                                <p className={classes.Animate_Text}>
+                                    <a href="https://github.com/laxanarana1513/travelwild" target="_blank" rel="noopener noreferrer">
+                                        <img src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png" alt="" />
+                                    </a>
 
-                                <a href="https://laxanarana1513.github.io/travelwild/" target="_blank" rel="noopener noreferrer">
-                                    <img src="https://img.icons8.com/glyph-neue/30/ffffff/link.png" alt="" />
-                                </a>
-                            </p>
-                            <div className="dots">
-                                <span></span>
-                                <span></span>
-                                <span></span>
+                                    <a href="https://laxanarana1513.github.io/travelwild/" target="_blank" rel="noopener noreferrer">
+                                        <img src="https://img.icons8.com/glyph-neue/30/ffffff/link.png" alt="" />
+                                    </a>
+                                </p>
+                                <div className="dots">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className={classes.Title}>
+                            <img src={Work3} alt="" className={classes.Timg} />
+                            <div className={classes.Text}>
+                                {/* <h1>Lorem ipsum.</h1> */}
+                                <h2 className={classes.Animate_Text}>Burger Builder - Created with React JS</h2>
+                                <p className={classes.Animate_Text}>
+                                    <a href="https://github.com/laxanarana1513/burger_builder" target="_blank" rel="noopener noreferrer">
+                                        <img src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png" alt="" />
+                                    </a>
+
+                                    <a href="https://laxanarana1513.github.io/burger_builder/" target="_blank" rel="noopener noreferrer">
+                                        <img src="https://img.icons8.com/glyph-neue/30/ffffff/link.png" alt="" />
+                                    </a>
+                                </p>
+                                <div className="dots">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={classes.Title}>
+                            <img src={Work4} alt="" className={classes.Timg} />
+                            <div className={classes.Text}>
+                                {/* <h1>Lorem ipsum.</h1> */}
+                                <h2 className={classes.Animate_Text}>Housify-House-Price-Predictor: Used to predict House Prices</h2>
+                                <p className={classes.Animate_Text}>
+                                    <a href="https://github.com/laxanarana1513/Housify_House_Price_Predictor" target="_blank" rel="noopener noreferrer">
+                                        <img src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png" alt="" />
+                                    </a>
+
+                                    {/* <a href="" target="_blank" rel="noopener noreferrer">
+                                        <img src="https://img.icons8.com/glyph-neue/30/ffffff/link.png" alt="" />
+                                    </a> */}
+                                </p>
+                                <div className="dots">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
                             </div>
                         </div>
                     </div>
 
+                    <div className="row">
 
-                    <div className={classes.Title}>
-                        <img src={Work3} alt="" className={classes.Timg} />
-                        <div className={classes.Text}>
-                            {/* <h1>Lorem ipsum.</h1> */}
-                            <h2 className={classes.Animate_Text}>Burger Builder - Created with React JS</h2>
-                            <p className={classes.Animate_Text}>
-                                <a href="https://github.com/laxanarana1513/burger_builder" target="_blank" rel="noopener noreferrer">
-                                    <img src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png" alt="" />
-                                </a>
+                        <div className={classes.Title}>
+                            <img src={Work1} alt="" className={classes.Timg} />
+                            <div className={classes.Text}>
+                                {/* <h1>Lorem ipsum.</h1> */}
+                                <h2 className={classes.Animate_Text}>My Portfolio Website - Created with React JS</h2>
+                                <p className={classes.Animate_Text}>
+                                    <a href="https://github.com/laxanarana1513/my-portfolio" target="_blank" rel="noopener noreferrer">
+                                        <img src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png" alt="" />
+                                    </a>
 
-                                <a href="https://laxanarana1513.github.io/burger_builder/" target="_blank" rel="noopener noreferrer">
-                                    <img src="https://img.icons8.com/glyph-neue/30/ffffff/link.png" alt="" />
-                                </a>
-                            </p>
-                            <div className="dots">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={classes.Title}>
-                        <img src={Work1} alt="" className={classes.Timg} />
-                        <div className={classes.Text}>
-                            {/* <h1>Lorem ipsum.</h1> */}
-                            <h2 className={classes.Animate_Text}>My Portfolio Website - Created with React JS</h2>
-                            <p className={classes.Animate_Text}>
-                                <a href="" target="_blank" rel="noopener noreferrer">
-                                    <img src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png" alt="" />
-                                </a>
-
-                                <a href="" target="_blank" rel="noopener noreferrer">
-                                    <img src="https://img.icons8.com/glyph-neue/30/ffffff/link.png" alt="" />
-                                </a>
-                            </p>
-                            <div className="dots">
-                                <span></span>
-                                <span></span>
-                                <span></span>
+                                    <a href="https://laxanarana1513.github.io/my-portfolio/" target="_blank" rel="noopener noreferrer">
+                                        <img src="https://img.icons8.com/glyph-neue/30/ffffff/link.png" alt="" />
+                                    </a>
+                                </p>
+                                <div className="dots">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
                             </div>
                         </div>
                     </div>
