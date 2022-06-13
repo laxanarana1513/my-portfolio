41bcf1f40a0b4b1fd0ca54791a838e5f74043e12
Laxana Rana <laxanarana1513@gmail.com>
2022-06-13 18:55:19 +0530

    commits

--- src/components/About/GrabCV/GrabCV.js
+++ src/components/About/GrabCV/GrabCV.js
@@ -7,19 +7,21 @@ const grabCV = () => {
         <section className={classes.Stats} >
 
             <div className="container">
-                
+
                 <div className="row">
                     <Fade bottom>
-                    <div className={classes.Cv}>
-                        <h3>Check Out My Resume</h3>
-                        <button type="button" className="btn btn-lg">
-                            <img src="https://img.icons8.com/ios/30/ffffff/open-resume.png" alt="" />
-                            Grab A Copy
-                        </button>
-                    </div>
+                        <div className={classes.Cv}>
+                            <h3>Check Out My Resume</h3>
+                            <button type="button" className="btn btn-lg">
+                                <a href="https://drive.google.com/file/d/1ooW8xedGRO_aAi3T45wtLZLjXqYRwV25/view?usp=sharing" target="_blank" without rel="noopener noreferrer">
+                                    <img src="https://img.icons8.com/ios/30/ffffff/open-resume.png" alt="" />
+                                    Grab A Copy
+                                </a>
+                            </button>
+                        </div>
                     </Fade>
                 </div>
-                
+
             </div>
         </section>
     )
